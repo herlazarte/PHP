dc381f37b07cb62ef165fbebdab03f5a0fba6921
CPSGEA - CB Mec Info Hernan Agustin Lazarte <herlazarte>
2024-11-05 11:55:49 -0300

    test

---
+++ PHP_MYSQL_POO/clase51.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $conexion=new mysqli("localhost","root","","pruebas");
+
+        if($conexion->connect_errno){
+            echo "fallo la conexion" . $conexion->connect_errno;
+        }
+        //
+        $conexion->set_charset("utf8");
+
+        $sql="SELECT * FROM artículos";
+
+        $resultado=$conexion->query($sql);
+
+        //si tenemos un error 
+        if($conexion->errno){
+            die($conexion->errno);
+        }
+
+        while($fila=$resultado->fetch_assoc()){
+            echo $fila['SECCION'] . "";
+            echo $fila['NOMBREARTICULO'] . "";
+            echo $fila['PAISDEORIGEN'] . "<br>";
+
+        }
+
+        $conexion->close();
+    
+    
+    ?>
+    
+</body>
+</html>
