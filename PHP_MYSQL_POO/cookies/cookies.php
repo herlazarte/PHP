1a55eb0c47b7ea3cd39d2c41eef69b6da878fcf2
herlazarte <agusmixherlazarte@gmail.com>
2024-11-12 17:31:15 -0300

    cookies

---
+++ PHP_MYSQL_POO/cookies/cookies.php
@@ -0,0 +1,11 @@
+<?php 
+
+setcookie('prueba','estoy en la cookies prueba');
+
+// settcokie('nombre de la cookie','contenido de la cookie',tiempo de expiracion,"ruta donde actuara la cookie");
+# setcookie('prueba','estoy en la cookies prueba',time()+30,"/carpeta/subcarpeta/");
+
+// Destruir cookies
+# setcookie('prueba',;'contenido',time()-1);
+
+?>
---
+++ PHP_MYSQL_POO/cookies/leerCookie.php
@@ -0,0 +1,11 @@
+<?php 
+ if(isset($_COOKIE["prueba"])){
+    echo $_COOKIE["prueba"];
+
+ }else{
+    echo "la cookie no se ha creado";
+ }
+
+
+
+?>
