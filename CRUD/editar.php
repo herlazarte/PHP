be115eec261df385f46626a5a420edf0d5f008ce
herlazarte <agusmixherlazarte@gmail.com>
2024-12-02 18:41:59 -0300

    t

--- CRUD/editar.php
+++ CRUD/editar.php
@@ -1,21 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <?php
 
 include("conexion.php");
 
-$id=$_GET["id"];
-$nom=$_GET["nom"];
-$ape=$_GET["ape"];
-$dir=$_GET["dir"];
+if(!isset($_POST["actualizar"])){
+    $id=$_GET["id"];
+    $nom=$_GET["nom"];
+    $ape=$_GET["ape"];
+    $dir=$_GET["dir"];
 
-
-
-
-
-
-
-
-
-
+}else{
+    $id=$_POST["id"];
+    $nom=$_POST["nom"];
+    $ape=$_POST["ape"];
+    $dir=$_POST["dir"];
+    $sql="UPDATE datos_usuarios SET Nombre=:miNom, Apellido=:miApe, Direccion=:miDir WHERE id=:miId";
+    $resultado=$base->prepare($sql);
+    $resultado->execute(array(":miId"=>$id,":miNom"=>$nom,":miApe"=>$ape,":miDir"=>$dir));
+    header("location:index.php");
+}
 
 
 ?>
+<h1>Editar</h1>
+<form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
+    <input type="hidden" name="id" value="<?php echo $id?>">
+    <label for="nombre">Nombre</label>
+    <input type="text" name="nom" id="nombre" value="<?php echo $nom?>">
+    <label for="apellido">Apellido</label>
+    <input type="text" name="ape" id="apellido" value="<?php echo $ape?>">
+    <label for="direccion">Direccion</label>
+    <input type="text" name="dir" id="direccion" value="<?php echo $dir?>">
+    <input type="submit" name="actualizar" value="Actualizar">
+</form>
+    
+</body>
+</html>
--- CRUD/index.php
+++ CRUD/index.php
@@ -16,46 +16,59 @@
     $conexion=$base->query("SELECT * FROM datos_usuarios");
 
     $registros=$conexion->fetchAll(PDO::FETCH_OBJ);
+
+    if(isset($_POST["cr"])){
+      $nombre=$_POST["Nom"];
+      $apellido=$_POST["Ape"];
+      $direccion=$_POST["Dir"];
+      $sql="INSERT INTO datos_usuarios (Nombre,Apellido,Direccion) VALUES (:nom,:ape,:dir)";
+      $resultado=$base->prepare($sql);
+      $resultado->execute(array(":nom"=>$nombre,":ape"=>$apellido,":dir"=>$direccion));
+
+      header("location:index.php");
+    }
   
   ?>
 
 
 <h1>CRUD<span class="subtitulo">Create Read Update Delete</span></h1>
 
-  <table width="50%" border="0" align="center">
-    <tr >
-      <td class="primera_fila">Id</td>
-      <td class="primera_fila">Nombre</td>
-      <td class="primera_fila">Apellido</td>
-      <td class="primera_fila">Dirección</td>
-      <td class="sin">&nbsp;</td>
-      <td class="sin">&nbsp;</td>
-      <td class="sin">&nbsp;</td>
-    </tr> 
-   
-		<?php 
-      foreach($registros as $persona):?>
+<form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
+    <table width="50%" border="0" align="center">
+      <tr >
+        <td class="primera_fila">Id</td>
+        <td class="primera_fila">Nombre</td>
+        <td class="primera_fila">Apellido</td>
+        <td class="primera_fila">Direccion</td>
+        <td class="sin">&nbsp;</td>
+        <td class="sin">&nbsp;</td>
+        <td class="sin">&nbsp;</td>
+      </tr> 
     
-   	<tr>
-      <td><?php echo $persona->id ?></td>
-      <td><?php echo $persona->Nombre?></td>
-      <td><?php echo $persona->Apellido?></td>
-      <td><?php echo $persona->Direccion?></td>
- 
-      <td class="bot"><a href="delete.php?id=<?php echo $persona->id?>"><input type='button' name='del' id='del' value='Borrar'></a></td>
+      <?php 
+        foreach($registros as $persona):?>
+      
+      <tr>
+        <td><?php echo $persona->id ?></td>
+        <td><?php echo $persona->Nombre?></td>
+        <td><?php echo $persona->Apellido?></td>
+        <td><?php echo $persona->Direccion?></td>
+  
+        <td class="bot"><a href="delete.php?id=<?php echo $persona->id?>"><input type='button' name='del' id='del' value='Borrar'></a></td>
 
-      <td class='bot'><a href="editar.php?id=<?php echo $persona->id?> & nom=<?php echo $persona->Nombre?> & ape=<?php echo $persona->Apellido?> & dir=<?php echo $persona->Direccion?>"><input type='button' name='up' id='up' value='Actualizar'></a></td>
-    </tr>
+        <td class='bot'><a href="editar.php?id=<?php echo $persona->id?> & nom=<?php echo $persona->Nombre?> & ape=<?php echo $persona->Apellido?> & dir=<?php echo $persona->Direccion?>"><input type='button' name='up' id='up' value='Actualizar'></a></td>
+      </tr>
 
-    <?php endforeach;?>
-    
-	<tr>
-	<td></td>
-      <td><input type='text' name='Nom' size='10' class='centrado'></td>
-      <td><input type='text' name='Ape' size='10' class='centrado'></td>
-      <td><input type='text' name=' Dir' size='10' class='centrado'></td>
-      <td class='bot'><input type='submit' name='cr' id='cr' value='Insertar'></td></tr>    
-  </table>
+      <?php endforeach;?>
+      
+      <tr>
+      <td></td>
+          <td><input type='text' name='Nom' size='10' class='centrado'></td>
+          <td><input type='text' name='Ape' size='10' class='centrado'></td>
+          <td><input type='text' name=' Dir' size='10' class='centrado'></td>
+          <td class='bot'><input type='submit' name='cr' id='cr' value='Insertar'></td></tr>    
+    </table>
+</form>
 
 <p>&nbsp;</p>
 </body>
