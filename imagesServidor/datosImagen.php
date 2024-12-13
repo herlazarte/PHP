1f6f48f3a75e0ec229d2842b4e92b6f9f81c308f
CPSGEA - CB Mec Info Hernan Agustin Lazarte <herlazarte>
2024-12-13 07:59:48 -0300

    valido el tamaño y tipo del archivo

--- imagesServidor/datosImagen.php
+++ imagesServidor/datosImagen.php
@@ -1,13 +1,27 @@
 <?php
-    if(isset($_FILES['imagen'])){
         $nombre_imagen = $_FILES['imagen']['name'];
         $tipo_imagen = $_FILES['imagen']['type'];
         $tamano_imagen = $_FILES['imagen']['size'];
+
+        if($tamano_imagen<=3000000){
+            
+            if($tipo_imagen=='image/jpeg' || $tipo_imagen=='image/jpg' || $tipo_imagen=='image/png' || $tipo_imagen=='image/gif'){
+                $carpeta_destino =$_SERVER['DOCUMENT_ROOT'].'/images/';
+                move_uploaded_file($_FILES['imagen']['tmp_name'], $carpeta_destino.$nombre_imagen);
+                echo "El archivo se ha subido correctamente";
+
+            }else{
+                echo "El archivo no es una imagen";
+            }	
+            
+        }else{
+            echo "El archivo es muy grande";
+        }
         
-        $carpeta_destino =$_SERVER['DOCUMENT_ROOT'].'/images/';
-        move_uploaded_file($_FILES['imagen']['tmp_name'], $carpeta_destino.$nombre_imagen);
-    }
-    
+        
+
+
+            
    
 
 ?>
